7c6787049e2eb2293b62bf44f651532e77b3cd15
Dora <domonshieh@gmail.com>
2021-07-06 01:08:57 +0800

    Initial Commit

---
+++ Foodish/SecondViewController.swift
@@ -0,0 +1,20 @@
+
+import UIKit
+
+class SecondViewController: UIViewController
+{
+
+    
+    @IBOutlet weak var NumberTextField: UITextField!
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+     
+    }
+    
+    
+
+}
---
+++ Foodish/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodish
+//
+//  Created by 謝孟芩 on 2021/7/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
