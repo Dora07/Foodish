8069fd4752088fdbb346b260059a9bf53033daf4
Dora <domonshieh@gmail.com>
2021-07-06 14:38:59 +0800

    textfield 限制只能打數字及限制字數

--- Foodish/SecondViewController.swift
+++ Foodish/SecondViewController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController
+class SecondViewController: UIViewController, UITextFieldDelegate
 {
 
     
@@ -11,10 +11,33 @@ class SecondViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        NumberTextField.delegate = self
      
     }
-    
-    
+   
+    //Delegate驗證欄位
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+    {
+        //限制只能打數字
+        if textField == NumberTextField
+        {
+            
+            let NumOfWords = string.count +
+               NumberTextField.text!.count - range.length
 
+            
+            if NumOfWords > 3  {
+                return false
+            }
+            
+            
+            let allowedCharacters = "1234567890"
+             let allowedCharcterSet = CharacterSet(charactersIn: allowedCharacters)
+             let typedCharcterSet = CharacterSet(charactersIn: string)
+             return allowedCharcterSet.isSuperset(of: typedCharcterSet)
+    
+        }
+        
+        return true
+    }
 }
