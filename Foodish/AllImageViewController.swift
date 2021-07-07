570bc4846b7496f295e72011fcbacc563364fa3b
Dora <domonshieh@gmail.com>
2021-07-08 01:11:52 +0800

    切換頁面

---
+++ Foodish/AllImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AllImageViewController.swift
+//  Foodish
+//
+//  Created by 謝孟芩 on 2021/7/8.
+//
+
+import UIKit
+
+class AllImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
