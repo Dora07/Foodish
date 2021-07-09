7032a5547c87b1ba4334b88f3cf30a423188d927
Dora <domonshieh@gmail.com>
2021-07-09 15:58:43 +0800

    call API 取得食物圖片

--- Foodish/OneImageViewController.swift
+++ Foodish/OneImageViewController.swift
@@ -6,12 +6,39 @@ class OneImageViewController: UIViewController
     var NumberOfImage : String?
 
     @IBOutlet weak var NumberYouChose: UILabel!
+    @IBOutlet weak var FoodImge: UIImageView!
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         NumberYouChose.text = NumberOfImage
+        TakeImage()
     }
+    
+    
+    func TakeImage() {
+      
+            //欲讀取資料url
+            let urlStr = "https://foodish-api.herokuapp.com/images/burger/burger\( NumberOfImage!).jpg"
+            if let url = URL(string: urlStr) {
+                //於其他執行緒open file, read content
+                URLSession.shared.dataTask(with: url) { (data, response, error) in
+                    if let data = data {
+                        //主執行緒執行 display content
+                        DispatchQueue.main.async {
+                            self.FoodImge.image = UIImage(data: data)
+                        }
+                    }
+                }.resume() //執行task
+            }
+            
+        }
+    
+    
+    
+    
+    
+    
     
     
     
